0d3e194d2552a7db3e6f8fc7afff59998575f816
Karansinh Inamdar <inamdarkaransinh@gmail.com>
2024-10-06 05:38:37 +0530

    JavaCrux

---
+++ src/Lecture2/Pattern_11.java
@@ -0,0 +1,38 @@
+package Lecture2;
+
+import java.util.Scanner;
+
+public class Pattern_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int star = 1;
+        //always remember the basics of loops
+        //here we are using the while loop.
+        //loops always needed three main components w
+        int space = n - 1;
+        while (row <= n) {
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+
+            int j = 1;
+            while (j <= star) {
+                if (j % 2 == 0) {
+                    System.out.print("! ");
+                } else {
+                    System.out.print("* ");
+                }
+                j++;
+            }
+
+            System.out.println();
+            space--;
+            row++;
+            star += 2;
+        }
+    }
+}
